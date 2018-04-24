16dc520d2c1a894b83b8393b1713223c2c8b07a0
이동원(Dongwon Lee) 대리 (주)두산 정보 <dongwon5.lee@doosan.com>
2018-04-24 16:00:20 +0900

    0424

--- ex4/webpack.config.js
+++ ex4/webpack.config.js
@@ -1,5 +1,7 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './src/index.js',
+    entry: ['react-hot-loader/patch', './src/index.js'] ,
 
     output: {
         path: __dirname + '/public/',
@@ -7,22 +9,29 @@ module.exports = {
     },
 
     devServer: {
+        hot: true,
         inline: true,
-        port: 7777,
-        contentBase: __dirname + '/public/'
+        host: '0.0.0.0',
+        port: 4000,
+        contentBase: __dirname + '/public/',
     },
 
-    module: {
-            loaders: [
-                {
-                    test: /\.js$/,
-                    loader: 'babel-loader',
-                    exclude: /node_modules/,
-                    query: {
-                        cacheDirectory: true,
-                        presets: ['es2015', 'react']
-                    }
+    module:{
+        loaders: [
+            {
+                test: /.js$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react'],
+                    plugins: ["react-hot-loader/babel"]
                 }
-            ]
-        }
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
